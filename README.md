23f6c401ff59190a1706972e5dd0dca9eef6f463
mchsiao602107 <mchsiao602107@gmail.com>
2022-07-05 16:59:07 +0800

    add example in r438Configurator.cc

--- README.md
+++ README.md
@@ -20,13 +20,13 @@ INET Simulation
 ### Project Setup
 
 ### Create r438 Project in OMNeT++
-File &rarr; New Project &rarr; OMNeT++ &rarr; OMNeT++ Project &rarr; Next &rarr; Next &rarr; Empty project with 'src' and 'simulations' folders &rarr; Finish
+- File&rarr;New Project&rarr;OMNeT++&rarr;OMNeT++ Project&rarr;Next&rarr;Next&rarr;Empty project with 'src' and 'simulations' folders&rarr;Finish
 
 ### Reference r438 Project to INET Project
-Right click r438 &rarr; Properties &rarr; Project References &rarr; Check out inet
+- Right click r438&rarr;Properties&rarr;Project References&rarr;Check out inet
 
 ### Reference Header Files Defined in INET Source Folder
-Right click r438 &rarr; Properties &rarr; OMNeT++ &rarr; Makemake &rarr; src &rarr; Options &rarr; Custom &rarr; Add following into "Code fragment to be inserted into Makefile":
+- Right click r438&rarr;Properties&rarr;OMNeT++&rarr;Makemake&rarr;src&rarr;Options&rarr;Custom&rarr;Add following into "Code fragment to be inserted into Makefile"
 ```bash
 CFLAGS += -I/home/mchsiao/omnetpp-6.0/INET_workspace/inet/src
 ```
--- src/r438Configurator.cc
+++ src/r438Configurator.cc
@@ -9,7 +9,8 @@ namespace inet
     {
         // TODO 1: Generate text files for forwarding tables.
         // TODO 2: Return Output() as GCL schedules.
-        // --------------------------------------------------
+        
+        // Reference: AlwaysOpenGateScheduleConfigurator
 
         // GCL schedules for all ports of all switches.
         auto output = new Output();
@@ -18,10 +19,24 @@ namespace inet
         for (auto port : input.ports) {
             // Iterate through all queues of a port.
             for (int gate_index = 0; gate_index < port->numGates; gate_index ++) {
+                
+                Output::Schedule *schedule = new Output::Schedule();
+                schedule->port = port;
+                schedule->gateIndex = gate_index;
+                schedule->cycleStart = 0;
+                schedule->cycleDuration = gateCycleDuration;
 
+                Output::Slot slot;
+                slot.start = 0;
+                slot.duration = gateCycleDuration;
+
+                schedule->slots.push_back(slot);
+                output->gateSchedules[port].push_back(schedule);
             }
         }
 
+        for (auto application : input.applications)
+            output->applicationStartTimes[application] = 0;
 
         return output;
     }
