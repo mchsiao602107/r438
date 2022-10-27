88dc28c2e6c43eee9943b151a3dad1e51c28342a
Hank Lee <r10922046@csie.ntu.edu.tw>
2022-10-27 18:10:24 +0800

    exp.sh only print result if (O1 or O2) > 0

--- exp.sh
+++ exp.sh
@@ -5,8 +5,6 @@ O2_gt_zero_cnt_round_1=0
 O1_gt_zero_cnt_round_2=0
 O2_gt_zero_cnt_round_2=0
 
-rm -f exp_round_2_results
-
 echo "Execute $n times of all-the-way-script.sh"
 for ((i=0; i<$n; i++))
 do	
@@ -18,26 +16,32 @@ do
 	# Check round-1 results
 	O1_sim=$(cat sim-round-1-result | tail -n 4 | head -n 1 | awk '{print $9}')
 	O2_sim=$(cat sim-round-1-result | tail -n 3 | head -n 1 | awk '{print $9}')
-	if [[ $O1_sim -ne 0 ]];then
-		((O1_gt_zero_cnt_round_1++))
-	fi
-	if [[ $O2_sim -ne 0 ]];then
-		((O2_gt_zero_cnt_round_1++))
+	if [[ $O1_sim -ne 0 ]] || [[ $O2_sim -ne 0 ]];then
+		if [[ $O1_sim -ne 0 ]];then
+			((O1_gt_zero_cnt_round_1++))
+		fi
+		if [[ $O2_sim -ne 0 ]];then
+			((O2_gt_zero_cnt_round_1++))
+		fi
+		# Print result
+		cat sim-round-1-result
+		echo ""
 	fi
 
 	# Check round-2 results
 	O1_sim=$(cat sim-round-2-result | tail -n 4 | head -n 1 | awk '{print $9}')
 	O2_sim=$(cat sim-round-2-result | tail -n 3 | head -n 1 | awk '{print $9}')
-	if [[ $O1_sim -ne 0 ]];then
-		((O1_gt_zero_cnt_round_2++))
+	if [[ $O1_sim -ne 0 ]] || [[ $O2_sim -ne 0 ]];then
+		if [[ $O1_sim -ne 0 ]];then
+			((O1_gt_zero_cnt_round_2++))
+		fi
+		if [[ $O2_sim -ne 0 ]];then
+			((O2_gt_zero_cnt_round_2++))
+		fi
+		# Print result
+		cat sim-round-2-result
+		echo ""
 	fi
-	if [[ $O2_sim -ne 0 ]];then
-		((O2_gt_zero_cnt_round_2++))
-	fi
-
-	# Show round-2 results
-	cat sim-round-2-result | tail -n 4
-	echo ""
 done
 
 echo -e "\nSummary of O1/O2 (sim) after $n times of all-the-way-script.sh"
